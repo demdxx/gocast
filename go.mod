b7e70ffeef5e945ec0c8eb4b6e54b2b4ac72fd3f
Dmitry Ponomarev <demdxx@gmail.com>
2022-01-28 21:07:25 +0100

    up dependencies

--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
 module github.com/demdxx/gocast
 
-go 1.14
+go 1.16
 
 require (
 	github.com/pkg/errors v0.9.1
-	github.com/stretchr/testify v1.5.1
+	github.com/stretchr/testify v1.7.0
 )
--- struct_test.go
+++ struct_test.go
@@ -14,6 +14,7 @@ func TestStructGetSetFieldValue(t *testing.T) {
 
 	assert.NoError(t, SetStructFieldValue(&st, "Name", "TestName"), "set Name field value")
 	assert.NoError(t, SetStructFieldValue(&st, "Value", int64(127)), "set Value field value")
+	assert.Error(t, SetStructFieldValue(&st, "UndefinedField", int64(127)), "set UndefinedField field value must be error")
 
 	name, err := StructFieldValue(st, "Name")
 	assert.NoError(t, err, "get Name value")
@@ -22,4 +23,7 @@ func TestStructGetSetFieldValue(t *testing.T) {
 	value, err := StructFieldValue(st, "Value")
 	assert.NoError(t, err, "get Value value")
 	assert.Equal(t, int64(127), value)
+
+	_, err = StructFieldValue(st, "UndefinedField")
+	assert.Error(t, err, "get UndefinedField value must be error")
 }
